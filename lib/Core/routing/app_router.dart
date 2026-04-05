5ec41d43418e4a21ed4c396d966ac7c0b3336ce2
Asmaa Abdelfattah <asmaa.fattah431@gmail.com>
2026-04-05 13:43:41 +0200

    add router, colors , textstyles , on boarding screen

---
+++ lib/Core/routing/app_router.dart
@@ -0,0 +1,19 @@
+import 'package:doctor_app/Core/routing/routes.dart';
+import 'package:doctor_app/Features/onboarding/onboarding_screen.dart';
+import 'package:flutter/material.dart';
+
+class AppRouter {
+
+  Route? generateRoute(RouteSettings settings) {
+    switch (settings.name) {
+      case Routes.onBoardingScreen:
+        return MaterialPageRoute(builder: (context) => const OnboardingScreen());
+      // case Routes.loginScreen:
+      //   return MaterialPageRoute(builder: (context) => const LoginScreen());
+      default:
+        return MaterialPageRoute(builder: (context) => const Scaffold(
+          body: Center(child: Text('No route found')),
+        ));
+    }
+  }
+}
